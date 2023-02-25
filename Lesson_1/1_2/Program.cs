0ceee8eca6a4ae64dfea7d800f7c2681aae6f9ce
Yulia Pestova <yul.pestowa@yandex.ru>
2023-02-25 19:44:52 +0500

    Task 1_2

---
+++ Lesson_1/1_2/Program.cs
@@ -0,0 +1,47 @@
+﻿// Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+// 3 -> Среда
+// 5 -> Пятница
+
+string s_a = Console.ReadLine ()!;
+
+if (s_a == "1");
+
+else if (s_a == "1")
+{
+                    Console.WriteLine("понедельник");
+}
+
+else if (s_a == "2")
+{
+                    Console.WriteLine("вторник");
+}
+
+else if (s_a == "3")
+{
+                    Console.WriteLine("среда");
+}
+
+else if (s_a == "4")
+{
+                    Console.WriteLine("четверг");
+}
+
+else if (s_a == "5")
+{
+                    Console.WriteLine("пятница");
+}
+
+else if (s_a == "6")
+{
+                    Console.WriteLine("суббота");
+}
+
+else if (s_a == "7")
+{
+                    Console.WriteLine("ВС");
+}
+
+else
+{
+                    Console.WriteLine("ERR");
+}
---
+++ Lesson_1/1_3/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+else if (s_a == "5")
+{
+                    Console.WriteLine("пятница");
+}
+
+else if (s_a == "6")
+{
+                    Console.WriteLine("суббота");
+}
+
+else if (s_a == "7")
+{
+                    Console.WriteLine("ВС");
+}
+
+else
+{
+                    Console.WriteLine("ERR");
+}
