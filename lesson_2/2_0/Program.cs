ed00f8cabf1f1d5c54bb937a45afa3809bec01ad
Yulia Pestova <yul.pestowa@yandex.ru>
2023-03-02 23:17:08 +0500

    Task 2_0

---
+++ lesson_2/2_0/Program.cs
@@ -0,0 +1,14 @@
+﻿// 1. Напишите программу, которая принимает на вход трёхзначное 
+//число и на выходе показывает последнюю цифру этого числа.
+//456 -> 6
+//782 -> 2
+//918 -> 8
+
+int TakeNum(int num)
+{
+                    Console.WriteLine(num);
+                    return num % 10;
+}
+int result = TakeNum(new Random().Next(100, 1000));
+Console.WriteLine(result);
+//Console.WriteLine (num % 10);
